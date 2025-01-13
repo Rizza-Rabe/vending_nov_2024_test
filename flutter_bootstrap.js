3d599f4e914ef27fa852e240524525aaef40e51e
Rizza Nobleza <rizzabhb24024@gmail.com>
2025-01-13 19:55:54 +0800

    2025 Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "948708100"
+    serviceWorkerVersion: "1049329406"
   }
 });
